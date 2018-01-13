21badaeb3b72b75a8ec61c05be8c9f3d6946ea03
brdtrpp <trapp2357@gmail.com>
2018-01-13 14:46:00 -0500

    git init

---
+++ imports/startup/client/index.js
@@ -0,0 +1,9 @@
+import '/imports/styles/bootstrap.scss';
+import '/imports/styles/app.scss';
+
+import '/imports/templates';
+import '/imports/scripts';
+
+import './routes.js';
+
+import './vendor.js';
