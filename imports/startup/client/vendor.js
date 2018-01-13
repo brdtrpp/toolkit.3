2090acb10dc4f816104cebbd33218e3ccd003828
brdtrpp <trapp2357@gmail.com>
2018-01-13 18:04:52 -0500

    removed conflicting imports

--- imports/startup/client/vendor.js
+++ imports/startup/client/vendor.js
@@ -54,64 +54,64 @@ import '../../../node_modules/whirl/dist/whirl.min.css';
 // // Nestable
 // import '../../../node_modules/nestable/jquery.nestable.js';
 // // Sortable
-// window.sortable = require('html5sortable');
-// // Range slider
-// import '../../../node_modules/bootstrap-slider/dist/css/bootstrap-slider.min.css';
-// import '../../../node_modules/bootstrap-slider/dist/bootstrap-slider.min.js';
-// // Colorpicker
-// import '../../../node_modules/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.css';
-// import '../../../node_modules/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js';
-// // Chosen
-// import '../../../node_modules/chosen-js/chosen.css';
-// import '../../../node_modules/chosen-js/chosen.jquery.js';
-// // Input Mask
-// import '../../../node_modules/inputmask/dist/jquery.inputmask.bundle.js';
-// // Select2
-// import '../../../node_modules/select2/dist/css/select2.min.css';
-// import '../../../node_modules/select2-bootstrap-theme/dist/select2-bootstrap.min.css';
-// import '../../../node_modules/select2/dist/js/select2.full.min.js';
-// // Datetimepicker
-// import '../../../node_modules/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css';
-// import '../../../node_modules/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js';
-// // Wysiwig
-// import '../../../node_modules/bootstrap-wysiwyg/css/style.css';
-// import '../../../node_modules/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js';
-// // TagsInput
-// import '../../../node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.css';
-// import '../../../node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js';
-// // Filestyle
-// import '../../../node_modules/bootstrap-filestyle/src/bootstrap-filestyle.min.js';
-// // Form Validation
-// import '../../../node_modules/jquery-validation/dist/jquery.validate.min.js';
-// import '../../../node_modules/jquery-validation/dist/additional-methods.js';
-// // Parsley (validation)
-// import '../../../node_modules/parsleyjs/dist/parsley.min.js';
-// // Wizard (jquery.steps)
-// import '../../../node_modules/jquery-steps/build/jquery.steps.min.js';
-// // xEditable
-// import '../../../node_modules/x-editable/dist/bootstrap3-editable/css/bootstrap-editable.css';
-// import '../../../node_modules/x-editable/dist/bootstrap3-editable/js/bootstrap-editable.min.js';
-// // Image cropper
-// import '../../../node_modules/cropper/dist/cropper.min.css';
-// import '../../../node_modules/cropper/dist/cropper.min.js';
-// // Knob charts
-// import '../../../node_modules/jquery-knob/dist/jquery.knob.min.js';
-// // d3 charts
-// import '../../../node_modules/d3/build/d3.min.js';
-// // Chart.js
-// import '../../../node_modules/chart.js/dist/Chart.min.js';
-// // Chartist
-// import '../../../node_modules/matchmedia/index.js';
-// import '../../../node_modules/chartist/dist/chartist.css';
-// window.Chartist = require('../../../node_modules/chartist/dist/chartist.min.js');
-// // Rickshaw
-// import '../../../node_modules/rickshaw/rickshaw.css';
-// window.Rickshaw = require('../../../node_modules/rickshaw/rickshaw.js');
-// // Morris.js
-// window.Raphael = require('raphael');
-// import '../../../node_modules/morris.js.so/morris.css';
-// import '../../../node_modules/morris.js.so/morris.js';
-// // Datatables
+window.sortable = require('html5sortable');
+// Range slider
+import '../../../node_modules/bootstrap-slider/dist/css/bootstrap-slider.min.css';
+import '../../../node_modules/bootstrap-slider/dist/bootstrap-slider.min.js';
+// Colorpicker
+import '../../../node_modules/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.css';
+import '../../../node_modules/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js';
+// Chosen
+import '../../../node_modules/chosen-js/chosen.css';
+import '../../../node_modules/chosen-js/chosen.jquery.js';
+// Input Mask
+import '../../../node_modules/inputmask/dist/jquery.inputmask.bundle.js';
+// Select2
+import '../../../node_modules/select2/dist/css/select2.min.css';
+import '../../../node_modules/select2-bootstrap-theme/dist/select2-bootstrap.min.css';
+import '../../../node_modules/select2/dist/js/select2.full.min.js';
+// Datetimepicker
+import '../../../node_modules/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css';
+import '../../../node_modules/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js';
+// Wysiwig
+import '../../../node_modules/bootstrap-wysiwyg/css/style.css';
+import '../../../node_modules/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js';
+// TagsInput
+import '../../../node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.css';
+import '../../../node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js';
+// Filestyle
+import '../../../node_modules/bootstrap-filestyle/src/bootstrap-filestyle.min.js';
+// Form Validation
+import '../../../node_modules/jquery-validation/dist/jquery.validate.min.js';
+import '../../../node_modules/jquery-validation/dist/additional-methods.js';
+// Parsley (validation)
+import '../../../node_modules/parsleyjs/dist/parsley.min.js';
+// Wizard (jquery.steps)
+import '../../../node_modules/jquery-steps/build/jquery.steps.min.js';
+// xEditable
+import '../../../node_modules/x-editable/dist/bootstrap3-editable/css/bootstrap-editable.css';
+import '../../../node_modules/x-editable/dist/bootstrap3-editable/js/bootstrap-editable.min.js';
+// Image cropper
+import '../../../node_modules/cropper/dist/cropper.min.css';
+import '../../../node_modules/cropper/dist/cropper.min.js';
+// Knob charts
+import '../../../node_modules/jquery-knob/dist/jquery.knob.min.js';
+// d3 charts
+import '../../../node_modules/d3/build/d3.min.js';
+// Chart.js
+import '../../../node_modules/chart.js/dist/Chart.min.js';
+// Chartist
+import '../../../node_modules/matchmedia/index.js';
+import '../../../node_modules/chartist/dist/chartist.css';
+window.Chartist = require('../../../node_modules/chartist/dist/chartist.min.js');
+// Rickshaw
+import '../../../node_modules/rickshaw/rickshaw.css';
+window.Rickshaw = require('../../../node_modules/rickshaw/rickshaw.js');
+// Morris.js
+window.Raphael = require('raphael');
+import '../../../node_modules/morris.js.so/morris.css';
+import '../../../node_modules/morris.js.so/morris.js';
+// Datatables
 // import '../../../node_modules/datatables.net-bs/css/dataTables.bootstrap.css';
 // $.fn.dataTable = require('datatables.net-bs')(window, $);
 // require('datatables.net-buttons')(window, $);
@@ -123,25 +123,25 @@ import '../../../node_modules/whirl/dist/whirl.min.css';
 // require('datatables.net-buttons/js/buttons.flash.js')(window, $); // Flash file export
 // require('datatables.net-buttons/js/buttons.print.js')(window, $); // Print view button
 // import '../../../node_modules/dataTables.fontAwesome/dataTables.fontAwesome.css';
-// // jqGrid
+// jqGrid
 // import '../../../node_modules/jqGrid/css/ui.jqgrid.css';
 // import '../../../node_modules/jqGrid/js/i18n/grid.locale-en.js';
 // import '../../../node_modules/jqgrid/js/jquery.jqGrid.min.js';
 // import '../../../node_modules/components-jqueryui/themes/smoothness/jquery-ui.css';
-// // Google Maps
-// import '../../../node_modules/jquery.gmap/jquery.gmap.min.js';
-// // Fullcalendar
-// window.moment = require('moment');
-// import '../../../node_modules/fullcalendar/dist/fullcalendar.min.css';
-// import '../../../node_modules/fullcalendar/dist/fullcalendar.min.js';
-// import '../../../node_modules/fullcalendar/dist/gcal.min.js';
-// // Flatdoc (markdown)
-// window.marked = require('marked');
-// import '../../../node_modules/flatdoc/flatdoc.js';
-// // Fastclick
-// import '../../../node_modules/fastclick/lib/fastclick.js';
-// // jqCloud
-// import '../../../node_modules/jqcloud2/dist/jqcloud.min.css';
-// import '../../../node_modules/jqcloud2/dist/jqcloud.min.js';
+// Google Maps
+import '../../../node_modules/jquery.gmap/jquery.gmap.min.js';
+// Fullcalendar
+window.moment = require('moment');
+import '../../../node_modules/fullcalendar/dist/fullcalendar.min.css';
+import '../../../node_modules/fullcalendar/dist/fullcalendar.min.js';
+import '../../../node_modules/fullcalendar/dist/gcal.min.js';
+// Flatdoc (markdown)
+window.marked = require('marked');
+import '../../../node_modules/flatdoc/flatdoc.js';
+// Fastclick
+import '../../../node_modules/fastclick/lib/fastclick.js';
+// jqCloud
+import '../../../node_modules/jqcloud2/dist/jqcloud.min.css';
+import '../../../node_modules/jqcloud2/dist/jqcloud.min.js';
 // // jQueryUI mobile touch
 // import '../../../node_modules/jqueryui-touch-punch/jquery.ui.touch-punch.min.js';
